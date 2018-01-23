0e32d32d97b59662f21bdf2286d48b488a32cc2b
awyand <awyand@users.noreply.github.com>
2018-01-23 10:47:59 -0500

    Update main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -597,7 +597,7 @@ $(document).ready(function() {
     }
 
     // If current page is 1, disable previous button
-    if (currentPage === 1) {3
+    if (currentPage === 1) {
       $(".page-link-previous").parent().addClass("disabled");
     } else {
       $(".page-link-previous").parent().removeClass("disabled");
